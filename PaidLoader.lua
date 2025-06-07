4cafc7b60c50704a23546a723d30a52b775b06cd
Forexium <150202229+Forexium@users.noreply.github.com>
2025-06-07 15:07:35 -0700

    Update PaidLoader.lua

--- PaidLoader.lua
+++ PaidLoader.lua
@@ -1,21 +1,29 @@
 getgenv().script_key = getgenv()['script_key']
 
 local Players = game:GetService("Players")
+local UserInputService = game:GetService("UserInputService")
 local LocalPlayer = Players.LocalPlayer or Players.PlayerAdded:Wait()
 
 local GamesTable = {
-    [4777817887] = "https://api.luarmor.net/files/v3/loaders/52bde2f8f3575360a08cb188c4d58760.lua", 
-    [6931042565] = "https://api.luarmor.net/files/v3/loaders/5f64ce462bf8ca3880ec58844926ef6b.lua",
+    [4777817887] = "https://api.luarmor.net/files/v3/loaders/0f66ccdb8f14d7fe0e363f0a178def1d.lua", 
+    [6931042565] = "https://api.luarmor.net/files/v3/loaders/a87a227e3c56f160f20e224c1f4ada84.lua",
 }
 
 local GameId = game.GameId
-local Executor = identifyexecutor()
+local Executor = identifyexecutor and identifyexecutor() or "Unknown"
 local Link = GamesTable[GameId]
+local HorribleExploits = {"Solara", "Xeno"}
 
 if not Link then
-    LocalPlayer:Kick("Not Supported")
+    LocalPlayer:Kick("Use Velocity or Swift")
 elseif not script_key then
     LocalPlayer:Kick("No identified Key")
+elseif GameId == 6061766680 and table.find(HorribleExploits, Executor) then
+    LocalPlayer:Kick("Use Velocity or Swift.")
+elseif Executor == "Fluxus" then
+    LocalPlayer:Kick("Use Velocity or Swift.")
+elseif game.PlaceId == 132756651286184 or game.PlaceId == 124842177627298 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/cb930180d28b181b0cf87d0efe29a784.lua"))()
 end
 
 loadstring(game:HttpGet(Link))()
